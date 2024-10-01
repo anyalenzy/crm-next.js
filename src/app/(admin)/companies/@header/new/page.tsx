6c8d0180e4d4f2edbfac1c63ce35721aee327f6d
anyalenzy <anya.dudnik.91@gmail.com>
2024-10-01 23:54:49 +0300

    finished

--- src/app/(admin)/companies/@header/new/page.tsx
+++ src/app/(admin)/companies/@header/new/page.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import React from 'react';
-import Header from '@/app/components/header';
+import CompanyForm from '@/app/components/company-form';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return <Header>Add new company</Header>;
+  return (
+    <div className="py-6 px-10">
+      <CompanyForm />
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import AddCompanyButton from './components/add-company-button';
-import StatusLabel, { Status } from './components/status-label';
-
-const Home = () => {
+export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
-      <AddCompanyButton />
     </main>
   );
-};
-export default Home;
+}
